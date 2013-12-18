928b067b7294f9f9532596f0b4674c14f5f2f090
Rizart Dokollari <r.dokollari@acg.edu>
2013-12-18 19:33:40 +0200

    Prints to TAG(MainListActivity class) tht title of all arrays.

--- src/com/rizartdokollari/blogreader/MainListActivity.java
+++ src/com/rizartdokollari/blogreader/MainListActivity.java
@@ -1,15 +1,25 @@
 package com.rizartdokollari.blogreader;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 
@@ -22,11 +32,30 @@ public class MainListActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 
-		GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
-		getBlogPostsTask.execute();
+		if (isNetwokAvailable()) {
+			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			getBlogPostsTask.execute();
+		} else {
+			Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG)
+					.show();
+		}
 
 		// String message = getString(R.string.no_items);
 		// Toast.makeText(this, message , Toast.LENGTH_LONG).show();
+	}
+
+	private boolean isNetwokAvailable() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+		boolean isAvailable = false;
+		if (networkInfo != null && networkInfo.isConnected()) { // check if
+																// network is
+																// Present
+			isAvailable = true;
+		}
+
+		return isAvailable;
 	}
 
 	@Override
@@ -52,9 +81,32 @@ public class MainListActivity extends ListActivity {
 				HttpURLConnection httpURLConnection = (HttpURLConnection) blogFeedUrl
 						.openConnection();
 				httpURLConnection.connect();
-
 				responseCode = httpURLConnection.getResponseCode();
-				Log.e(TAG, "Response Code: " + responseCode);
+
+				if (responseCode == HttpURLConnection.HTTP_OK) { // successful
+																	// response
+					InputStream inputStream = httpURLConnection
+							.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);
+					int contectLength = httpURLConnection.getContentLength();
+					char[] charArray = new char[contectLength];
+					reader.read(charArray);
+					String responseData = new String(charArray);
+
+					JSONObject jsonResponse = new JSONObject(responseData);
+					String status = jsonResponse.getString("status");
+					Log.v(TAG, status);
+
+					JSONArray jsonPosts = jsonResponse.getJSONArray("posts");
+					for (int i = 0; i < jsonPosts.length(); i++) {
+						JSONObject jsonPost = jsonPosts.getJSONObject(i);
+						String title = jsonPost.getString("title");
+						Log.v(TAG, "Post " + i + ": " + title);
+					} // end for
+				} else {
+					Log.e(TAG, "Unsuccessful HTTP Response Code: "
+							+ responseCode);
+				}
 			} catch (MalformedURLException e) {
 				Log.e(TAG, "MalformedURLException caught: ", e);
 			} catch (IOException e) {
