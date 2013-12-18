918d32822eae562fac8128411cd75e35daff0e72
Rizart Dokollari <r.dokollari@acg.edu>
2013-12-18 16:42:19 +0200

    Connect to Internet

--- src/com/rizartdokollari/blogreader/MainListActivity.java
+++ src/com/rizartdokollari/blogreader/MainListActivity.java
@@ -1,27 +1,29 @@
 package com.rizartdokollari.blogreader;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.ListActivity;
-import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 
-	private String[] mAndroidNames;
+	private String[] mBlogPostTitles;
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 
-		Resources resources = getResources();
-		setmAndroidNames(resources.getStringArray(R.array.funny_sentences));
-
-		// <> Specifies data type.
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, getmAndroidNames());
-		setListAdapter(adapter);
+		GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+		getBlogPostsTask.execute();
 
 		// String message = getString(R.string.no_items);
 		// Toast.makeText(this, message , Toast.LENGTH_LONG).show();
@@ -34,12 +36,45 @@ public class MainListActivity extends ListActivity {
 		return true;
 	}
 
+	// create a subclass
+	private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... arg0) { // ellipses zero or n
+															// arguments
+
+			int responseCode = -1;
+			try {
+				URL blogFeedUrl = new URL(
+						"http://blog.teamtreehouse.com/api/get_recent_summary/?count="
+								+ NUMBER_OF_POSTS);
+
+				HttpURLConnection httpURLConnection = (HttpURLConnection) blogFeedUrl
+						.openConnection();
+				httpURLConnection.connect();
+
+				responseCode = httpURLConnection.getResponseCode();
+				Log.e(TAG, "Response Code: " + responseCode);
+			} catch (MalformedURLException e) {
+				Log.e(TAG, "MalformedURLException caught: ", e);
+			} catch (IOException e) {
+				Log.e(TAG, "IOException caught: " + e.getMessage(), e);
+			} catch (Exception e) {
+				Log.e(TAG, "Exception caught: ", e);
+
+			}
+			return "Code: " + responseCode;
+
+		}
+
+	}
+
 	public String[] getmAndroidNames() {
-		return mAndroidNames;
+		return mBlogPostTitles;
 	}
 
 	public void setmAndroidNames(String[] mAndroidNames) {
-		this.mAndroidNames = mAndroidNames;
+		this.mBlogPostTitles = mAndroidNames;
 	}
 
 }
